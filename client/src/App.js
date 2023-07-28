87e74503a2773f549848a78a022805713870418e
cgonz <christoph.gonz@gmail.com>
2023-07-28 03:29:48 -0700

    starter client code

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+function App() {
+  return <></>;
+}
+
+export default App;
