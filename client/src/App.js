e7b9b7d1aed00930b151855cbb17d40c1ffce290
BreeAnn <breeannbond@gmail.com>
2023-07-30 21:25:33 -0700

    adjusted openMap API

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from "@apollo/client";
+// import { setContext } from "@apollo/client/link/context";
 
 function App() {
   return <></>;
