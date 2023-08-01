30da0cef52d797805542669d9175a7757cfa7ab4
cgonz <christoph.gonz@gmail.com>
2023-07-31 20:27:48 -0700

    changed mongodb name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/programming-thoughts"
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/wanderlust'
 );
 
 module.exports = mongoose.connection;
