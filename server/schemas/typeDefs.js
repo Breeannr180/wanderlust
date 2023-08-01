bb7941fb956316cfa789dcbc519af624d636de60
tbohn2 <tanner.bohn@yahoo.com>
2023-07-31 21:09:29 -0700

    Created typeDefs for models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,27 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    savedLocations: [Location]
+}
+
+type Location {
+    _id: ID!
+    name: String!
+    lat: Int!
+    long: Int!
+    savedFeatures: [Feature]
+}
+
+type Feature {
+    _id: ID!
+    name: String!
+    dist: Int!
+    rate: Int!
+    wikidata: String!
+}
+`;
 
 module.exports = typeDefs;
