641543fc6284e6c134cdf764e70773222a142ab0
tbohn2 <tanner.bohn@yahoo.com>
2023-08-01 18:59:23 -0700

    Added query resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,15 +4,22 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    user: async () => {},
-    location: async () => {},
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('savedLocations')
+    },
+    location: async (parent, { name }) => {
+      return Location.findOne({ name }).populate('savedFeatures')
+    },
+    feature: async (parent, { name }) => {
+      return Feature.findOne({ name })
+    },
   },
   Mutation: {
-    addUser: async () => {},
-    editUser: async () => {},
-    addLocation: async () => {},
-    editLocation: async () => {},
-    login: async () => {},
+    addUser: async () => { },
+    editUser: async () => { },
+    addLocation: async () => { },
+    editLocation: async () => { },
+    login: async () => { },
   },
 };
 
