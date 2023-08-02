ae824bfa88cc85a5015a2252c336f3d549992549
jorgemunoz7 <jamunoz9@asu.edu>
2023-08-01 19:25:34 -0700

    .js
    Added .js file for "Login Page"

---
+++ client/Login Page/.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Slideshow = () => {
+  return (
+    <div className="slideshow">
+      {/* Add your random destination slideshow here */}
+      {/* Example: */}
+      <img src="destination1.jpg" alt="Destination 1" />
+      <img src="destination2.jpg" alt="Destination 2" />
+      <img src="destination3.jpg" alt="Destination 3" />
+    </div>
+  );
+};
+
+export default Slideshow;
