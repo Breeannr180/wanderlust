fbde9f9cd2e9013c2f9fecd1107e26721a80c368
cgonz <christoph.gonz@gmail.com>
2023-08-01 18:28:50 -0700

    imported nav into header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from './Nav';
 
 const Header = () => {
   return (
     <div className='header'>
       <p>WanderLust</p>
+      <Nav />
     </div>
   );
 };
