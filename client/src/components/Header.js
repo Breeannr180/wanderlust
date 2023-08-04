bc820cc98fe50dff6a130a802e408f0cf23830c1
BreeAnn <breeannbond@gmail.com>
2023-08-03 19:04:42 -0700

    added logo to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Nav from '../components/Nav';
+import logo from '../images/logo.png';
 
 const Header = () => {
   return (
     <nav className='navbar bg-neutral'>
       <div className='navbar-start'>
+      <img src={logo} alt='logo' className='logo' />
         <button className='btn btn-active bg-base-300 text-xl p2'>
-          <Link to='/'>Travel Wanderlust</Link>
+          <Link to='/'>Travel Wanderlust  </Link>
         </button>
       </div>
       <Nav />
