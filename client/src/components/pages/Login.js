0abb708cdd7abd69b20dad06210e97c15e57da8c
cgonz <christoph.gonz@gmail.com>
2023-08-03 19:45:17 -0700

    added styles and switching functionality to login

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,16 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Login = () => {
+  const [loggingIn, setLoggingIn] = useState(true);
+  const handleToggle = () => {
+    setLoggingIn(!loggingIn);
+  };
+
   return (
     <div>
-      <div className='login'>
-        <h2>Login</h2>
-        <form>
-          <input type='text' placeholder='Email' required />
-          <input type='password' placeholder='Password' required />
-          <button type='submit'>Log In</button>
-          <button type='button'>Sign Up Instead</button>
-        </form>
+      <div className='card'>
+        {loggingIn ? (
+          <div className='card-body'>
+            <h2 className='card-title'>Login</h2>
+            <form className='form-control'>
+              <input
+                className='input input-bordered'
+                type='text'
+                placeholder='Email'
+                required
+              />
+              <input
+                className='input input-bordered'
+                type='password'
+                placeholder='Password'
+                required
+              />
+              <div className='card-actions'>
+                <button className='btn btn-wide' type='submit'>
+                  Log In
+                </button>
+              </div>
+            </form>
+
+            <button type='button' onClick={handleToggle}>
+              Sign Up Instead
+            </button>
+          </div>
+        ) : (
+          <div className='card-body'>
+            <h2 className='card-title'>Sign Up</h2>
+            <form className='form-control'>
+              <input
+                className='input input-bordered'
+                type='text'
+                placeholder='Email'
+                required
+              />
+              <input
+                className='input input-bordered'
+                type='password'
+                placeholder='Password'
+                required
+              />
+              <div className='card-actions'>
+                <button className='btn btn-wide' type='submit'>
+                  Sign Up
+                </button>
+              </div>
+            </form>
+            <button type='button' onClick={handleToggle}>
+              Already have an account? Log In Here
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
