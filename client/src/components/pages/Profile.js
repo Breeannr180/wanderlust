08d4f0f6b97c19a9597304d5ca34c6fd18fa7ce4
tbohn2 <tanner.bohn@yahoo.com>
2023-08-05 15:54:35 -0700

    User data fetched used profileContext

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import LocationCard from '../elements/LocationCard';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../../utils/queries';
+import { useParams } from 'react-router-dom';
+import { useProfileContext } from '../../utils/GlobalState';
 
-const Profile = ({ username, savedLocations }) => {
+const Profile = () => {
+
+  const [queryUser, { error, data }] = useQuery(QUERY_USER);
+  const { profile } = useProfileContext();
+
+  const fetchData = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await queryUser({
+        variables: { _id: profile._id },
+      });
+
+      console.log(data)
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  fetchData()
+
   return (
     <div>
+      <h1>${username}'s saved locations</h1>
       <div className='card-content'>
         <div className='content'>
           {!savedLocations ? (
