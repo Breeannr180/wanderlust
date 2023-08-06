84a649c3c794eb65fc83bf8f9741a008adaa8f46
cgonz <christoph.gonz@gmail.com>
2023-08-06 03:16:36 -0700

    added to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
     extend: {},
   },
   plugins: [require('daisyui')],
