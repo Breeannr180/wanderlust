828abe2f6ebb7117027331e5c29ed01e5cd7e686
tbohn2 <tanner.bohn@yahoo.com>
2023-08-05 17:57:41 -0700

    Added button to link to location page

--- client/src/components/elements/LocationCard.js
+++ client/src/components/elements/LocationCard.js
@@ -1,11 +1,17 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const LocationCard = ({ name }) => {
+const LocationCard = ({ key, name }) => {
+
+
+
   return (
     <div className='card'>
       <div className='card-header'>
         <h3 className='card-header-title'>{name}</h3>
+        <Link to={'./location/' + key}>
+          <button>See Saved Features</button>
+        </Link>
       </div>
     </div>
   );
