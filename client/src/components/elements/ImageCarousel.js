0942622453a6fc92063a51bc261a4ddd35e4d35f
Christopher <christoph.gonz@gmail.com>
2023-08-06 23:54:21 -0700

    Moved the image carousel to be its own component

---
+++ client/src/components/elements/ImageCarousel.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import {
+  parisImage,
+  newYorkImage,
+  singaporeImage,
+  sydneyImage,
+  dubaiImage,
+  japanImage,
+  romeImage,
+} from '../../images/carouselImages';
+
+const imageSize = {
+  width: '500px',
+  height: '300px',
+};
+
+const ImageCarousel = () => {
+  return (
+    <div className='carousel-container mt-8'>
+      <Carousel
+        showArrows={true}
+        infiniteLoop={true}
+        showThumbs={true}
+        thumbWidth={300}
+        showStatus={true}
+        autoPlay={true}
+        interval={3000} // Set the interval (in milliseconds) for automatic carousel sliding
+      >
+        <div className='flex flex-row items-center justify-center'>
+          <img src={parisImage} alt='Paris' style={imageSize} />
+        </div>
+        <div className='flex flex-row items-center justify-center'>
+          <img src={newYorkImage} alt='New York' style={imageSize} />
+        </div>
+        <div className='flex flex-row items-center justify-center'>
+          <img src={singaporeImage} alt='Singapore' style={imageSize} />
+        </div>
+        <div className='flex flex-row items-center justify-center'>
+          <img src={sydneyImage} alt='Sydney' style={imageSize} />
+        </div>
+        <div className='flex flex-row items-center justify-center'>
+          <img src={romeImage} alt='Rome' style={imageSize} />
+        </div>
+        <div className='flex flex-row items-center justify-center'>
+          <img src={japanImage} alt='Japan' style={imageSize} />
+        </div>
+        <div className='flex flex-row items-center justify-center'>
+          <img src={dubaiImage} alt='Dubai' style={imageSize} />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default ImageCarousel;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,21 +1,5 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-
-import {
-  parisImage,
-  newYorkImage,
-  singaporeImage,
-  sydneyImage,
-  dubaiImage,
-  japanImage,
-  romeImage,
-} from '../../images/carouselImages';
-
-const imageSize = {
-  width: '500px',
-  height: '300px',
-};
+import ImageCarousel from '../elements/ImageCarousel';
 
 const Home = () => {
   return (
@@ -23,39 +7,8 @@ const Home = () => {
       <div className='home'>
         <h1>WanderLust</h1>
         <h2>Explore the world with us!</h2>
-      </div>
-      <div className='carousel-container mt-8'>
-        <Carousel
-          showArrows={true}
-          infiniteLoop={true}
-          showThumbs={true}
-          thumbWidth={300}
-          showStatus={true}
-          autoPlay={true}
-          interval={3000} // Set the interval (in milliseconds) for automatic carousel sliding
-        >
-          <div className='flex flex-row items-center justify-center'>
-            <img src={parisImage} alt='Paris' style={imageSize} />
-          </div>
-          <div className='flex flex-row items-center justify-center'>
-            <img src={newYorkImage} alt='New York' style={imageSize} />
-          </div>
-          <div className='flex flex-row items-center justify-center'>
-            <img src={singaporeImage} alt='Singapore' style={imageSize} />
-          </div>
-          <div className='flex flex-row items-center justify-center'>
-            <img src={sydneyImage} alt='Sydney' style={imageSize} />
-          </div>
-          <div className='flex flex-row items-center justify-center'>
-            <img src={romeImage} alt='Rome' style={imageSize} />
-          </div>
-          <div className='flex flex-row items-center justify-center'>
-            <img src={japanImage} alt='Japan' style={imageSize} />
-          </div>
-          <div className='flex flex-row items-center justify-center'>
-            <img src={dubaiImage} alt='Dubai' style={imageSize} />
-          </div>
-        </Carousel>
+
+        <ImageCarousel />
       </div>
     </div>
   );
