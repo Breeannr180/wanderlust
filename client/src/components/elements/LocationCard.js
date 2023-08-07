ed0d4fe03e93380698588f227f52a41e561ba15c
tbohn2 <tanner.bohn@yahoo.com>
2023-08-06 18:41:20 -0700

    Implemented locationId key

--- client/src/components/elements/LocationCard.js
+++ client/src/components/elements/LocationCard.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const LocationCard = ({ key, name }) => {
-
-
+const LocationCard = ({ name, locationId }) => {
 
   return (
     <div className='card'>
       <div className='card-header'>
         <h3 className='card-header-title'>{name}</h3>
-        <Link to={'./location/' + key}>
+        <Link to={`/location/${locationId}`}>
           <button>See Saved Features</button>
         </Link>
       </div>
