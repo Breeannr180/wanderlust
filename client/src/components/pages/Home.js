41f1400bbda4f31b06a296bc67937fb65fe52a03
BreeAnn <breeannbond@gmail.com>
2023-08-06 21:38:59 -0700

    added carousel to home.js

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+import parisImage from '../../images/Paris.jpg';
+import newYorkImage from '../../images/New York.jpg';
+import singaporeImage from '../../images/Singapore.jpg';
+import sydneyImage from '../../images/Sydney.jpg';
+import dubaiImage from '../../images/Dubai.jpg';
+import japanImage from '../../images/Japan.jpg';
+import romeImage from '../../images/Rome.jpg';
+
+const imageSize = {
+  width: '500px', 
+  height: '300px', 
+  // objectFit: 'cover', 
+};
 
 const Home = () => {
   return (
@@ -8,31 +23,41 @@ const Home = () => {
         <h1>WanderLust</h1>
         <h2>Explore the world with us!</h2>
       </div>
-      <div className='carousel-container'>
+      <div class='carousel-container'>
         <Carousel
           showArrows={true}
           infiniteLoop={true}
-          showThumbs={false}
-          showStatus={false}
+          showThumbs={true}
+          thumbWidth={300}
+          showStatus={true}
           autoPlay={true}
-          interval={5000} // Set the interval (in milliseconds) for automatic carousel sliding
+          interval={3000} // Set the interval (in milliseconds) for automatic carousel sliding
         >
-          <div>
-            <img src='path/to/image1.jpg' alt='Image 1' />
-            {/* You can add any additional content you want to display for each slide */}
+           <div className='flex flex-row items-center justify-center'>
+            <img src={parisImage} alt='Paris' style={imageSize} />
           </div>
-          <div>
-            <img src='path/to/image2.jpg' alt='Image 2' />
+          <div className='flex flex-row items-center justify-center'>
+            <img src={newYorkImage} alt='New York' style={imageSize} />
           </div>
-          <div>
-            <img src='path/to/image3.jpg' alt='Image 3' />
+          <div className='flex flex-row items-center justify-center'>
+            <img src={singaporeImage} alt='Singapore' style={imageSize} />
           </div>
-          {/* Add more divs with images for more slides */}
+          <div className='flex flex-row items-center justify-center'>
+            <img src={sydneyImage} alt='Sydney' style={imageSize} />
+          </div>
+          <div className='flex flex-row items-center justify-center'>
+            <img src={romeImage} alt='Rome' style={imageSize} />
+          </div>
+          <div className='flex flex-row items-center justify-center'>
+            <img src={japanImage} alt='Japan' style={imageSize} />
+          </div>
+          <div className='flex flex-row items-center justify-center'>
+            <img src={dubaiImage} alt='Dubai' style={imageSize} />
+          </div>
         </Carousel>
-    </div>
+      </div>
     </div>
   );
 };
 
 export default Home;
-
