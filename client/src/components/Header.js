afc8b75ef8fcb17a8e4b74642b9a03c5aac05d64
Christopher <christoph.gonz@gmail.com>
2023-08-08 18:49:07 -0700

    header flexwrap

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className='header'>
       <div className='header-content'>
-        <nav className='navbar bg-neutral'>
+        <nav className='navbar bg-neutral flex flex-wrap'>
           <div className='navbar-start'>
             <img
               src={logo}
